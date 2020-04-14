6e6b40bd2f1ca734c098d244b2b1c1939ef97e03
vivekburman <vivekburman1997@gmail.com>
2020-04-14 16:12:26 +0530

    feature/TSN-0004: Added support for row/column deletion and video player

--- webapp/src/components/profile.component/profile.jsx
+++ webapp/src/components/profile.component/profile.jsx
@@ -1,31 +1,18 @@
-import React, { Component } from 'react';
-import NavMenuComponent from './functional.component/navMenu';
-import navMenuIcon from '../../images/nav-menu.svg';
-class ProfileComponent extends Component {
-    constructor (props) {
-        super();
-        this.handleNavMenuClick = this.handleNavMenuClick.bind(this);
-        this.state = {
-            navMenuClicked: false,
-            hasResized: -1
-        };
-    }
-    handleNavMenuClick (e) {
-        this.setState({ hasResized: this.props.hasWindowResized, navMenuClicked: !this.state.navMenuClicked });
-    }
-    render () {
-        const { navMenuClicked, hasResized } = this.state;
-        let navMenuDirection = navMenuClicked ? 'column': 'row',
-            { hasWindowResized } = this.props;
-        if (hasWindowResized !== hasResized) {
-            navMenuDirection = ''
-        }
-        return (
-            <nav className="profile">
-                <img src={ navMenuIcon } alt="navMenu" className="icon-img navMenu" onClick={this.handleNavMenuClick}/>
-                <NavMenuComponent flexDirection={navMenuDirection}/>
-            </nav>
-        );
-    }
+import React from 'react';
+import bell from '../../images/bell.svg';
+import user from '../../images/user.svg';
+const ProfileComponent = () => {
+	return (
+		<nav className="profile">
+			<ul>
+				<li>
+					<img src={ bell } alt="notifications" className="icon-img" />
+				</li>
+				<li>
+					<img src={user} alt="notifications" className="icon-img" />
+				</li>
+        </ul>
+		</nav>
+	);
 }
 export default ProfileComponent;
--- webapp/src/components/text.editor.component/tool.js
+++ webapp/src/components/text.editor.component/tool.js
@@ -3,14 +3,13 @@ import Table from "@editorjs/table";
 import List from "@editorjs/list";
 import Code from "@editorjs/code";
 import LinkTool from "@editorjs/link";
-import Image from "@editorjs/image";
 import Header from "@editorjs/header";
 import Quote from "@editorjs/quote";
 import Marker from "@editorjs/marker";
 import Delimiter from "@editorjs/delimiter";
 import InlineCode from "@editorjs/inline-code";
+import SimpleVideo from '../../_internal/simple-video/dist/bundle';
 import SimpleImage from "@editorjs/simple-image";
-
 export const EDITOR_JS_TOOLS = {
   embed: Embed,
   table: Table,
@@ -18,10 +17,10 @@ export const EDITOR_JS_TOOLS = {
   list: List,
   code: Code,
   linkTool: LinkTool,
-  image: Image,
+  image: SimpleImage,
+  video: SimpleVideo,
   header: Header,
   quote: Quote,
   delimiter: Delimiter,
-  inlineCode: InlineCode,
-  simpleImage: SimpleImage
+  inlineCode: InlineCode
 };
