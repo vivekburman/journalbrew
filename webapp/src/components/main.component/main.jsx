bc890f34b1163dc825b6c7a8ae91856ef0edd3a2
vivekburman <vivekburman1997@gmail.com>
2020-04-20 17:36:54 +0530

    made profile section for dashboard

--- webapp/src/components/main.component/main.jsx
+++ webapp/src/components/main.component/main.jsx
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
 import Accordian from '../accordian.component/accordian';
+import {Dashboard} from '../dashboard.component/dashboard';
 // import Advertisement from '../advertisement.component/advertisement';
 import TextEditor from '../text.editor.component/text.editor.component';
 import ShowFeed from '../show.feed.component/show.feed';
 import './main.component.scss';
 import { Switch, Route } from 'react-router-dom';
 class Main extends Component {
-    render() {
-        const { windowWidth } = this.props;
-        console.log(windowWidth);
-        return (
-            <main className="main">
-               <Switch>
-                   <Route exact path="/">
-                    <Accordian windowWidth= {windowWidth}/>
-                    <ShowFeed windowWidth= {windowWidth}/>
-                    {/* <Advertisement/> */}
-                   </Route>
-                   <Route exact path="/new-story">
-                    <TextEditor />
-                   </Route>
-               </Switch>
-            </main>
-        );
-    }
+  render() {
+    const { windowWidth } = this.props;
+    return (
+      <main className="main">
+        <Switch>
+          <Route exact path="/new-story">
+            <TextEditor />
+          </Route>
+          <Route exact path="/">
+            <Accordian windowWidth= {windowWidth}/>
+            <div className="main-feed">
+              <ShowFeed windowWidth= {windowWidth}/>
+            </div>
+            {/* <Advertisement/> */}
+          </Route>
+          <Route exact path="/user-profile">
+            <Accordian windowWidth= {windowWidth}/>
+            <div className="main-feed">
+              <Dashboard windowWidth= {windowWidth}/>
+            </div>
+            {/* <Advertisement/> */}
+          </Route>
+        </Switch>
+      </main>
+    );
+  }
 }
 export default Main;
