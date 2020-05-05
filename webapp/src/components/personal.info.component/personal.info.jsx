1f5030160204475315fab1448572409fe12d8020
vivekburman <vivekburman1997@gmail.com>
2020-05-05 21:28:01 +0530

    added skeleton to notifications and profile

--- webapp/src/components/personal.info.component/personal.info.jsx
+++ webapp/src/components/personal.info.component/personal.info.jsx
@@ -3,20 +3,29 @@ import StarRatingComponent from 'react-star-rating-component';
 import calendar from '../../images/calendar.svg';
 import './personal.info.component.scss';
 import {UserAvatar} from '../avatar.component/avatar'; 
-
+import Skeleton from 'react-loading-skeleton';
 export const PersonalInfo = (props) => {
   const { profileID, username, doj, rating, typeOfID } = props;
   return (
     <div className="personal-info-container">
       <div className="personal-info-details">
-        <h1 className="user-name">{username || 'DummyName'}</h1>
-        <div>
+        { username ? <h1 className="user-name">{username}</h1> : 
+         <div style={{ maxWidth: '150px' }}>
+           <Skeleton height={20}/>
+         </div> }
+        { doj ? <div>
           <img src={calendar} className="icon-img calendar-img" alt="calendar"/>
           <time className="time">{doj || 'Jan 15, 2019'}</time>
-        </div>
-        <StarRatingComponent name={'userrating'}value={rating||2} editing={false} starColor='yellow' emptyStarColor='lightgrey'/>
+        </div> : 
+        <div style={{ lineHeight: 1.5 }}>
+          <Skeleton width={65} />
+        </div> }
+        { rating ? <StarRatingComponent name={'userrating'} value={rating} editing={false}
+          starColor='yellow' emptyStarColor='lightgrey'/> : 
+          <Skeleton width={70} height={10}/>}
       </div>
-      <UserAvatar id={profileID} type={typeOfID} />
+      { (profileID && typeOfID) ? <UserAvatar id={profileID} type={typeOfID} /> 
+      : <Skeleton circle={true} height={100} width={100} />}
     </div>
   );
 }
