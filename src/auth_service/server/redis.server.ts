f1d8d37c387ad56db2f7c4085002cda2c8c19df8
Vivek Burman <vivekburman1997@gmail.com>
2022-03-26 10:58:50 +0530

    TSN--redis password moved

--- src/auth_service/server/redis.server.ts
+++ src/auth_service/server/redis.server.ts
@@ -5,8 +5,8 @@ let client: RedisClient;
 export const initRedis = () => {
     client =  redis.createClient({
         port: 6379,
-        host: '192.168.0.102',
-        password: 'journal_brew' 
+        host: process.env.REDIS_HOST,
+        password: process.env.REDIS_PASSWORD 
     });
     
     client.on("error", (err) => {
