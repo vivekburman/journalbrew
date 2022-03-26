578e61cb740565fc45e5d12abf0f3f13b9614886
Vivek Burman <vivekburman1997@gmail.com>
2022-03-26 15:31:27 +0530

    TSN--deleted old dist

--- webapp/dist/static/js/vendors.domelementtype.ba9bc839.chunk.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{322:function(t,e){t.exports={Text:"text",Directive:"directive",Comment:"comment",Script:"script",Style:"style",Tag:"tag",CDATA:"cdata",Doctype:"doctype",isTag:function(t){return"tag"===t.type||"script"===t.type||"style"===t.type}}}}]);
