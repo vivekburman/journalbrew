0b4ec582704c8fb16e6d7eb8d88d34b40d1872a2
Vivek Burman <vivekburman1997@gmail.com>
2022-04-06 10:12:59 +0530

    JB: removed logger redux in prod

--- webapp/src/store.js
+++ webapp/src/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './reducers/rootReducer';
 
-const middleWares = [logger];
+const middleWares = process.env.NODE_ENV === 'production' ? [] : [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
