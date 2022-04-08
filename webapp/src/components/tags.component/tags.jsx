fefad9e7a09243f3a52301cfb20428f8283c6139
Vivek Burman <vivekburman1997@gmail.com>
2022-04-08 20:28:55 +0530

    JB: minor fixes CSS

--- webapp/src/components/tags.component/tags.jsx
+++ webapp/src/components/tags.component/tags.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import './tags.component.scss';
 
+const getCapitalizedText = (str) => {
+  if (!str) return str;
+  return str[0].toUpperCase() + str.slice(1);
+}
+
+
 const createTags = (tags=[], readOnly, handleCrossClick) => {
+  
   const _handleCrossClick = (e) => {
     handleCrossClick(+e.target.getAttribute("data-index"));
   }
+
+
   return tags.map((item, index) => {
     if (readOnly) {
       return (
-        <span key={item} className="tag">{item}</span>
+        <span key={item} className="tag">{getCapitalizedText(item)}</span>
       );
     }
     else {
       return (
         <span key={item} className="tag tag-closeable">
-          <span title={item} className="tag-text">{item}</span>
+          <span title={item} className="tag-text">{getCapitalizedText(item)}</span>
           <i className="tag-cross" data-index={index} onClick={_handleCrossClick}>&times;</i>
         </span>
       );
