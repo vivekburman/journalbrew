41096d0bf525738090824dd27c06c5bbd29a80b8
Vivek Burman <vivekburman1997@gmail.com>
2022-04-10 00:12:35 +0530

    JB: css fix

--- webapp/dist/service-worker.js
+++ webapp/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.770286713ff44e651071ed69a2872b04.js"
+  "/precache-manifest.a5715f5477087d55d4d1dec2b1a5a478.js"
 );
 
 workbox.core.skipWaiting();
